7be61a8f8aa8735888f771c75ceb4b0ab83015b6
rohanpreetsingh8767-maker <rohanpreetsingh8767@gmail.com>
2026-01-17 16:38:07 +0530

    Updated QuizReviewPage.tsx

--- project/project/src/pages/teacher/QuizReviewPage.tsx
+++ project/project/src/pages/teacher/QuizReviewPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import { CheckCircle, XCircle, Clock, User, MessageSquare, Star } from 'lucide-react';
 
 interface QuizSubmission {
